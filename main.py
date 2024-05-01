9a9638157a9b3f4f120974e0f2e5219d3c000548
iiTzShaa <psnsharon@gmail.com>
2024-05-01 23:53:39 +0300

    add final project

--- main.py
+++ main.py
@@ -22,11 +22,11 @@ def get_spotify_uris(song_list, year):
         auth_manager=SpotifyOAuth(
             scope="playlist-modify-private",
             redirect_uri="http://example.com",
-            client_id="0ee16c30ed6a4205acd75549dc3e296c",
-            client_secret="8e9845d299a645a492cd5e7814f8b6c4",
+            client_id="YOUR CLIENT ID",
+            client_secret="YOUR CLIENT SECRET",
             show_dialog=True,
             cache_path="token.txt",
-            username="Sharon Shimon",
+            username="YOUR NAME",
         )
     )
 
@@ -35,9 +35,9 @@ def get_spotify_uris(song_list, year):
     for song in song_list:
         result = sp.search(q=f"track:{song} year:{year}", type="track")
         try:
-           uri = result["tracks"]["items"][0]["uri"]
+            uri = result["tracks"]["items"][0]["uri"]
 
-           songs_URI.append(uri)
+            songs_URI.append(uri)
         except IndexError:
             print(f"{song} doesn't exist in Spotify. Skipped.")
 
@@ -51,21 +51,19 @@ def create_playlist_from_song_list(song_uris, user_id, playlist_name):
         auth_manager=SpotifyOAuth(
             scope="playlist-modify-private",
             redirect_uri="http://example.com",
-            client_id="0ee16c30ed6a4205acd75549dc3e296c",
-            client_secret="8e9845d299a645a492cd5e7814f8b6c4",
+            client_id="YOUR CLIENT ID",
+            client_secret="YOUR CLIENT SECRET",
             show_dialog=True,
             cache_path="token.txt",
-            username="Sharon Shimon",
+            username="YOUR NAME",
         )
     )
     playlist = sp.user_playlist_create(user=user_id, name=playlist_name, public=False)
     sp.playlist_add_items(playlist_id=playlist['id'], items=song_uris)
 
-date ="2009-07-18"
+
+date = "2009-07-18"
 song_list = get_songs_form_web(date)
 year = "2009"
 song_uris, user_id = get_spotify_uris(song_list, year)
-create_playlist_from_song_list(song_uris,user_id, f"billboards hot 100s {date}")
-
-
-
+create_playlist_from_song_list(song_uris, user_id, f"billboards hot 100s {date}")
